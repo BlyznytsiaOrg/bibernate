8d389cb2c9d939b0166fde0dceecd5910c7f892c
Roman Novosad <roman.novosad@yahoo.com>
2024-02-11 10:34:24 +0100

    [BLZT-10] fix bug on foreign key constraint creation

--- core/src/main/java/io/github/blyznytsiaorg/bibernate/annotation/JoinColumn.java
+++ core/src/main/java/io/github/blyznytsiaorg/bibernate/annotation/JoinColumn.java
@@ -8,6 +8,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface JoinColumn {
-    String name() default "";
+    String name();
     ForeignKey foreignKey() default @ForeignKey(name = "");
 }
